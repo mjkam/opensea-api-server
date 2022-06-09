5dd1f6ac02e3e4c8d139a66667f7e780ac35034d
mjkam <mjkam@naver.com>
2022-06-09 21:52:56 +0900

    oinit

---
+++ src/main/java/com/mjkam/openseaapiserver/OpenseaApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.mjkam.openseaapiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenseaApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenseaApiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mjkam/openseaapiserver/OpenseaApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjkam.openseaapiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenseaApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
